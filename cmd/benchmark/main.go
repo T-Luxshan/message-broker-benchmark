1e8744243b43a31d08c06bcb70488381a3b3b8ee
T-Luxshan <luckybraveboys@gmail.com>
2026-04-12 01:27:02 +0530

    consumer producer modified

--- cmd/benchmark/main.go
+++ cmd/benchmark/main.go
@@ -4,7 +4,14 @@ import (
 	"fmt"
 	"log"
 	"message-broker-benchmark/internal/broker/rabbitmq"
+	"sync"
 	"time"
+)
+
+const (
+	totalMessages = 1000
+	producerCount = 10
+	messageSize   = 256
 )
 
 func main() {
@@ -22,29 +29,69 @@ func main() {
 		}
 	}(b)
 
+	// Wait for all messages consumed
+	var done sync.WaitGroup
+	done.Add(totalMessages)
+
+	// Consumer
 	go func() {
-
-		for i := 0; i < 20; i++ {
-
-			msg := fmt.Sprintf("message %d", i)
-
-			err := b.Publish([]byte(msg))
-			if err != nil {
-				log.Println(err)
-			}
-
-			fmt.Println("Sent:", msg)
-
-			time.Sleep(time.Second)
+		err := b.Consume(func(body []byte) {
+			done.Done()
+		})
+		if err != nil {
+			log.Fatal(err)
 		}
-
 	}()
 
-	err = b.Consume(func(body []byte) {
-		fmt.Println("Received:", string(body))
-	})
+	time.Sleep(time.Second) // ensure consumer ready
 
-	if err != nil {
-		log.Fatal(err)
+	start := time.Now()
+
+	// Producer wait group
+	var prodWg sync.WaitGroup
+	prodWg.Add(producerCount)
+
+	messagesPerProducer := totalMessages / producerCount
+
+	for p := 0; p < producerCount; p++ {
+
+		go func(pid int) {
+			defer prodWg.Done()
+
+			startIdx := pid * messagesPerProducer
+			endIdx := startIdx + messagesPerProducer
+
+			for i := startIdx; i < endIdx; i++ {
+
+				msg := generateMessage(i, messageSize)
+
+				err := b.Publish(msg)
+				if err != nil {
+					log.Println("publish error:", err)
+				}
+			}
+		}(p)
 	}
+
+	prodWg.Wait() // wait producers finish
+	done.Wait()   // wait all messages consumed
+
+	duration := time.Since(start)
+
+	fmt.Println("Total messages:", totalMessages)
+	fmt.Println("Producers:", producerCount)
+	fmt.Println("Message size:", messageSize)
+	fmt.Println("Time taken:", duration)
+	fmt.Println("Throughput:", float64(totalMessages)/duration.Seconds(), "msg/sec")
+}
+
+func generateMessage(i int, size int) []byte {
+	base := fmt.Sprintf("msg-%d-", i)
+
+	padding := size - len(base)
+	if padding < 0 {
+		padding = 0
+	}
+
+	return []byte(base + string(make([]byte, padding)))
 }
